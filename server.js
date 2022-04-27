dcc38f27b31d3af9b610d284a3a68cda220a51be
KennethKosling <kenkoz7164@gmail.com>
2022-04-27 19:21:59 -0400

    server file

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(require('./config/checkToken'));
+
+app.use('/users', require('./routes/users'));
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/items', ensureLoggedIn, require('./routes/items'));
+app.use('/orders', ensureLoggedIn, require('./routes/orders'));
+
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3090;
+
+app.listen(port, function() {
+  console.log(`We'll have flying cars in the year ${port}`)
+});
