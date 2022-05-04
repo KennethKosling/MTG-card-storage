9ebe43f6d80e871cd8ed207120da4fd669b5ae6f
KennethKosling <kenkoz7164@gmail.com>
2022-05-04 19:04:46 -0400

    Created User model

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar(){
   return(
     <div className={styles.NavBar}>
       <Link to="/cards" className={styles.Link}><h1 className={styles.NavBarItem}>Cards</h1></Link>
-      <h1 className={styles.NavBarItem}>Username</h1>
+      <h1 className={styles.NavBarUserName}>Username</h1>
       <Link to="/" className={styles.Link}><h1 className={styles.NavBarItem}>Sign Out</h1></Link>
     </div>
   )
